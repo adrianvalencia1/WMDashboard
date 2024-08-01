857c57c9555bdad30c8c52176a08d204cd038ec4
adrianvalencia1 <avalencia@semah.com>
2024-08-01 13:56:28 -0400

    changed port to 0.0.0.0

--- app.py
+++ app.py
@@ -22,4 +22,4 @@ app.layout = html.Div([
 server = app.server
 
 if __name__ == '__main__':
-    app.run(debug=False, port=os.getenv("PORT", default=5000))
+    app.run(debug=True, host="0.0.0.0", port=os.getenv("PORT", default=5000))
