056c087a029f213b19f383960a01716c0bde3a9f
adrianvalencia1 <avalencia@semah.com>
2024-08-01 12:55:16 -0400

    gave up on webapp.io

--- navbar.py
+++ navbar.py
@@ -1,7 +1,7 @@
 from dash import html
 import dash_bootstrap_components as dbc
 
-#LOGO = "https://www.satovsky.com/wp-content/uploads/2020/10/satovsky-new-logo.png"
+LOGO = "https://www.satovsky.com/wp-content/uploads/2020/10/satovsky-new-logo.png"
 
 def create_navbar():
     navbar = dbc.NavbarSimple(
@@ -9,19 +9,19 @@ def create_navbar():
             dbc.NavItem(dbc.NavLink("Home", href="/")),
             dbc.NavItem(dbc.NavLink("Monte Carlo Simulation", href="/monte-carlo-simulation")),
             dbc.NavItem(dbc.NavLink("Portfolio Optimization", href="/portfolio-optimization"))
-        ]
-        #brand=dbc.Container([
-        #        html.A(
-        #            dbc.Row([
-        #                dbc.Col(html.Img(src=LOGO, height="57px")),
-        #            ]),
-        #            href="https://satovsky.com",
-        #        )], 
-        #        style={'margin':'0'}
-        #    ),
-        #color="dark",
-        #dark=True,
-        #style={'margin':'0'}
+        ],
+        brand=dbc.Container([
+                html.A(
+                    dbc.Row([
+                        dbc.Col(html.Img(src=LOGO, height="57px")),
+                    ]),
+                    href="https://satovsky.com",
+                )], 
+                style={'margin':'0'}
+            ),
+        color="dark",
+        dark=True,
+        style={'margin':'0'}
     )
     
     return navbar
